6afdc8b295356a9c575ac5458c434998e200da72
Oskarnuz <oscar.nunez07@gmail.com>
2023-02-14 09:37:29 -0500

    Update the code

--- app.js
+++ app.js
@@ -2,28 +2,27 @@ const express = require('express');
 const app = express();
 
 const data = {
-		message: "Curso encontrado satisfactoriamente",
-		top:{
-			version_top: 27,
-			students: 10,
-			names: [
-				"Oscar", 
-				"Nicolas", 
-				"Jean", 
-				"Diego", 
-				"Camilo", 
-				"Michael",
-				"Andres",
-				"Sebastian",
-			]
-		}
+    message: "Curso encontrado satisfactoriamente",
+    top:{
+        version_top: 27,
+        students: 10,
+        names: [
+            "Oscar",
+            "Nicolas",
+            "Jean",
+            "Diego",
+            "Camilo",
+            "Michael",
+            "Andres",
+            "Sebastian",
+        ]
+    }
 }
 
 
 
 app.get('/my-first-endpoint', (req, res) => {
-    res.json(data);
-    res.sendStatus(200);
+    res.status(200).json(data);
     
 });
 
